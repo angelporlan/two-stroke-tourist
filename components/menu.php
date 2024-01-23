453e1f03cc26b3fe897b3d52d05e8ccca893d7cf
angelporlan <angeelporlan4@gmail.com>
2024-01-23 14:28:56 +0100

    f

---
+++ components/menu.php
@@ -0,0 +1,15 @@
+    <!-- Encabezado de la página -->
+    <header>
+        <h1>Two Stroke Tourist</h1>
+
+        <nav>
+        <ul>
+            <li><a href="#rutas">Rutas</a></li>
+            <li><a href="#alquiler">Alquiler</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+            <li><a href="#rutas">Iniciar Sesión</a></li>
+            <li><a href="#rutas">Crear Cuenta</a></li>
+        </ul>
+        </nav>
+    </header>
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alquiler de Motos y Rutas</title>
+    <link rel="stylesheet" href="./css/menu.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/footer.css">
+
+</head>
+<body>
+
+<?php include_once('./components/menu.php'); ?>
+
+   <main> <!-- Sección de Destacados / Intro -->
+    <section id="inicio">
+        <h2>Bienvenido a nuestro servicio de alquiler de motos</h2>
+        <p>Explora nuevas aventuras sobre dos ruedas. Ofrecemos una amplia selección de motocicletas para satisfacer tus necesidades.</p>
+        <!-- Agrega imágenes u otros elementos destacados aquí -->
+    </section>
+
+    <!-- Sección de Alquiler -->
+    <section id="alquiler">
+        <h2>Alquiler de Motos</h2>
+        <p>Selecciona la moto que se adapte a tu estilo y necesidades. ¡Haz tu reserva ahora!</p>
+        <!-- Incluye información sobre las motos disponibles y un formulario de reserva -->
+    </section>
+
+    <!-- Sección de Rutas -->
+    <section id="rutas">
+        <h2>Rutas Escénicas</h2>
+        <p>Descubre rutas emocionantes y paisajes impresionantes con nuestras sugerencias de rutas. ¡Prepárate para la aventura!</p>
+        <!-- Incluye información sobre las rutas disponibles y un mapa interactivo, si es posible -->
+    </section>
+
+    <!-- Sección de Contacto -->
+    <section id="contacto">
+        <h2>Contacto</h2>
+        <p>¿Tienes preguntas? ¿Listo para reservar? ¡Contáctanos!</p>
+        <!-- Incluye un formulario de contacto o información de contacto -->
+    </section>
+    </main>
+
+<?php include_once('./components/footer.php'); ?>
+
+</body>
+</html>
